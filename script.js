ed298f8f382b57f650f707de55697f0526cc7028
SOPHIA OLIVEIRA ACUNZO <40173049826@info.local>
2024-05-27 11:02:46 -0300

    feat: nao sei mais

--- script.js
+++ script.js
@@ -71,3 +71,108 @@ function mostrarProximoSlide () {
     banner.classList.add(slides[slideAtual])
 
 }
+
+function mostrarSlideAnterior() {
+    // Remover o slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        // Subtrair 1 na variavel slideAtual
+        slideAtual--
+    } else {
+        // Voltar para o ultimo slide
+        slideAtual = 2
+    }
+
+    // Mostrar slide de acordo com o slide atual
+    banner.classList.add(slides[slideAtual])
+}
+
+function selecionarSlide(indiceSlide) {
+    // Remove o slide atual
+    banner.classList.remove(slides[slideAtual])
+    
+    // Atualiza a variavel com o indice de slide selecionado
+    slideAtual = indiceSlide
+
+    // Mostra o slide selecionado e salvo na variavel slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+
+// Carregamento dinâmico dos cases
+let listaCases = [
+    
+]
+
+function renderizarCases() {
+    // Encontrar o elemento para inserir os cards
+    let containerCards = document.querySelector(".container-cards")
+
+    // Variavel para guardar o html dos cases montados
+    let template = ""
+
+    // Para cada case da listaCases
+    listaCases.forEach(cardCase => {
+        // Montar o html do card, passando os atributos do case
+        template += `<div class="card">
+            <img src=${ cardCase.imagem } alt="">
+            <p>${ cardCase.descricao }</p>
+            <button>Ver mais</button>
+        </div>`
+    })
+
+    // Inserir html dos cases montados no elemento container-cards
+    containerCards.innerHTML = template
+}
+
+function carregarCases() {
+    //metodo htp gete - read/leitura - serve para mostrar um item da lista de intens 
+    fetch("http://localhost:3000/cases")
+    .then( (resposta) => resposta.json() )
+    .then( (dadosTratados) => {
+        console.log(dadosTratados)
+        listaCases = dadosTratados
+        renderizarCases()
+    })
+}
+
+function solicitarOrcamento(event) {
+    //pegar os valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value  
+    let valorEmail = document.getElementById("campo-email").value  
+    let valorDescricao = document.getElementById("campo-texto").value  
+
+    console.log(valorNome, valorEmail, valorDescricao)
+    //otgsnizar os valores de um objeto
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+    //enviar a requisisicao para a api
+     fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+     })
+        //caso sucesso = limpar campos - mostrar um alert d sucesso (metodo http post -create/criar = cadastrar um novo registro (solicitacao de orcamento))
+.then(resposta => {
+    console.log(resposta);
+    //limpar
+    document.querySelector("#contato form").reset()
+    //mostrar alert
+    alert("solicitacao enviada com sucesso!!")
+
+})
+        //caso erro = mostrar alert de erro
+.catch(erro => {
+    console.log(erro);
+    //mostrar alert
+    alert("erro na requisicao")
+})    
+
+event.preventDefault()
+}
